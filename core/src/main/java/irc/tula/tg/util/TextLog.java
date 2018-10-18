147eac04aa1818acddfd03f779e920d5ba6008be
Evgeny Ilushin <zloy@helix-vms.milane>
2018-10-18 14:27:41 +0300

    Reply test fix

---
+++ core/src/main/java/irc/tula/tg/util/TextLog.java
@@ -0,0 +1,37 @@
+package irc.tula.tg.util;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
+public class TextLog {
+
+	static final String TS_PATTERN = "yyyy/MM/dd HH:mm:ss";
+	static final SimpleDateFormat tsformat = new SimpleDateFormat(TS_PATTERN);
+
+	@Getter
+	private final String location;
+
+	public TextLog(String location) {
+		this.location = location;
+	}
+
+	public synchronized void add(String text) {
+		try
+		{
+			try (FileOutputStream fo = new FileOutputStream(location, true)) {
+				String line = tsformat.format(new Date()) + " " + text;
+				fo.write(line.getBytes());
+			} catch (Exception e) {
+				log.error("TextLog: {}", e);
+				e.printStackTrace();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
