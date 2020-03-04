05e165605f5172e9cf591f474efdd1de18a9760b
evgeny-ilushin <evgeny.v.iulushin@gmail.com>
2020-03-04 10:04:27 +0300

    Fix zeros in calc output

--- scripts/calc.sh
+++ scripts/calc.sh
@@ -5,24 +5,31 @@ LOG=/tmp/.calc.log
 TEXT="$@"
 RES="OK"
 
+SCALE=2
+
+function FixZeros {
+    echo "$@" | awk '{printf "%08f\n", $0}' | sed '/\./ s/\.\{0,1\}0\{1,\}$//'
+#    echo "$@" | sed '/\./ s/\.\{0,1\}0\{1,\}$//'
+}
+
 echo -e "[$TEXT]" >> $LOG
 
 if [[ $TEXT == *"."* ]] || [[ $TEXT == *","* ]]; then
     RES=$(echo "$TEXT" | bc 2>&1 | head -n 1)
 else
-    RES=$(echo "scale=2; $TEXT" | bc 2>&1 | head -n 1)
+    RES=$(echo "scale=$SCALE; $TEXT" | bc 2>&1 | head -n 1)
 fi
 
 if [[ $RES == *":"* ]]; then
     RES=$(echo "$RES" | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 else
-    RES=$(echo "scale=2; $TEXT" | bc 2>&1 | head -n 1)
+    RES=$(echo "scale=$SCALE; $TEXT" | bc 2>&1 | head -n 1)
 fi
 
 if [ -z "$RES" ]; then
     echo "syntax error"
 else
-    echo "$RES"
+    FixZeros "$RES"
 fi
 
 echo -e "[$RES]" >> $LOG
