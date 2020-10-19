ead226d9103491c164670f91fb0d9ab97ef23701
evgeny-ilushin <evgeny.v.iulushin@gmail.com>
2020-10-19 12:02:00 +0300

    Add more salt

--- core/src/main/java/irc/tula/tg/core/plugin/Qify.java
+++ core/src/main/java/irc/tula/tg/core/plugin/Qify.java
@@ -4,9 +4,31 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Qify {
     private static final int MIN_WORD_LENGTH = 3;
+
+    private static final String vowels = "аеёиоуыэюя";
+    private static final HashMap<Character, Character> rules = new HashMap<Character, Character>();
+    static {
+        rules.put('а','я');
+        rules.put('о','ё');
+        rules.put('у','ю');
+        rules.put('ы','и');
+        rules.put('э','е');
+    }
+
+    private static final Random random = new Random();
+
+    private static int countVowels(String text) {
+        if (text != null) {
+            String vwls = text.replaceAll( "(?i)[^аеёиоуыэюяАЕЁИОУЫЭЮЯ]+", "" );
+            int t = text.length(), l = vwls.length();
+            return vwls.length();
+        }
+        return 0;
+    }
 
     public static String text(String text) {
         try {
@@ -40,7 +62,46 @@ public class Qify {
         return text;
     }
 
+    public static String text(String text, boolean skipSome) {
+        try {
+            StringBuilder res = new StringBuilder();
+            String word = null;
+            for (int i = 0; i < text.length(); i++) {
+                String cs = text.substring(i, i + 1);
+                if (StringUtils.isAlpha(cs)) {
+                    if (word == null) {
+                        word = "";
+                    }
+                    word += cs;
+                } else {
+                    if (word != null) {
+                        word = word(word, skipSome);
+                        res.append(word);
+                        word = null;
+                    }
+                    res.append(cs);
+                }
+            }
+            if (word != null) {
+                word = word(word, skipSome);
+                res.append(word);
+            }
+            return res.toString();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return text;
+    }
+
     public static String word(String word) {
+        String lcWord = word.toLowerCase().trim();
+        int numVwls = countVowels(lcWord);
+        boolean skipSome = numVwls > 2 && random.nextBoolean();
+        return word(word, skipSome);
+    }
+
+    public static String word(String word, boolean skipSome) {
         String source = word;
 
         if (word == null || word.length() < MIN_WORD_LENGTH) {
@@ -48,35 +109,51 @@ public class Qify {
         }
 
         word = word.trim();
-        String lcWord = word.toLowerCase().trim();
         Boolean isUcWord = StringUtils.isAllUpperCase(""+word.charAt(0));
-
-        String vowels = "аеёиоуыэюя";
-        HashMap<Character, Character> rules = new HashMap<Character, Character>();
-        rules.put('а', 'я');
-        rules.put('о', 'ё');
-        rules.put('у', 'ю');
-        rules.put('ы', 'и');
-        rules.put('э', 'е');
+        int wcCnt = 0;
 
         for (char letter : word.toCharArray()) {
             boolean isUc = Character.isUpperCase(letter);
             char lcLetter = Character.toLowerCase(letter);
             if (vowels.indexOf(lcLetter) != -1) {
-                if (rules.containsKey(lcLetter)) {
-                    word = (isUc? Character.toUpperCase(rules.get(lcLetter)) : rules.get(lcLetter)) + word.substring(1);
+                if (!skipSome) {
+                    if (rules.containsKey(lcLetter)) {
+                        word = (isUc ? Character.toUpperCase(rules.get(lcLetter)) : rules.get(lcLetter)) + word.substring(1);
+                    }
+                    break;
+                } else {
+                    skipSome = false;
+                    word = word.substring(1);
                 }
-                break;
             } else {
                 word = word.substring(1);
             }
+            wcCnt++;
         }
         return word.isEmpty() ? source : ((isUcWord?"Х":"х") + "у" + word);
     }
 
     @Test
     public static void test1() {
-        System.out.println(text("Привет, ты и дела, и еще Тектоническая плита"));
+
+        /*
+        String S = "Привет, ты и дела, и еще Тектоническая плита";
+        int t = countVowels(S);
+        t = countVowels("Тектоническая");
+        t = countVowels("ПЛИТА");
+        System.out.println(text(S));
+
+        System.out.println(text("Тектоническая", true));
+        System.out.println(text("Тектоническая", false));
+        */
+
+        System.out.println(text("в ходе прокурорских проверок возбуждено 10 уголовных дел о коррупции"));
+        System.out.println(text("я хотел купить и спрашивал: а разговаривать по ней можно"));
+        System.out.println(text("латынина обнищала совсем, плачется, грозится закрыть передачу"));
+        System.out.println("----");
+        System.out.println(text("в ходе прокурорских проверок возбуждено 10 уголовных дел о коррупции"));
+        System.out.println(text("я хотел купить и спрашивал: а разговаривать по ней можно"));
+        System.out.println(text("латынина обнищала совсем, плачется, грозится закрыть передачу"));
     }
 
     public static void main(String[] args) {
@@ -84,8 +161,7 @@ public class Qify {
         test1();
         if (1 == 1) {
             return;
-        }
-        */
+        }*/
         String text = String.join(" ", args);
         if (StringUtils.isNotBlank(text)) {
             String qtext = text(text);
@@ -96,6 +172,7 @@ public class Qify {
             }
         } else {
             System.err.println("Usage: Qify <TEXT>");
+            test1();
         }
     }
 }
