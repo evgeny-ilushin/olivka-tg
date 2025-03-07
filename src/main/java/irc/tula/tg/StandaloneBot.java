07709c09282fcf0fc6d48618217fc648d32e7523
unknown <evgeny.ilushin@gmail.com>
2025-03-07 18:08:27 +0300

    Rework replies - 2

--- src/main/java/irc/tula/tg/StandaloneBot.java
+++ src/main/java/irc/tula/tg/StandaloneBot.java
@@ -1,5 +1,6 @@
 package irc.tula.tg;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.pengrad.telegrambot.UpdatesListener;
@@ -291,9 +292,12 @@ public class StandaloneBot extends BotCore implements UpdatesListener, ChannelBo
 
     public boolean chanserv(Long chatId, Nickname nickName, String text, Message originalMessage) {
         boolean answered = false;
-        log.info("chanserv: ({}, {}, {}, {})", chatId, nickName, text, originalMessage == null? null : originalMessage.messageId());
+        Integer originalMessageId = originalMessage == null? null : originalMessage.messageId();
+        log.info("chanserv: ({}, {}, {}, {})", chatId, nickName, text, originalMessageId);
         String replyNickName = nickName.toString();
         Long from_id = nickName.getId();
+
+        callbacks.add(toJson(nickName));
 
         boolean ignoreMessage = getConfig().isIgnored(nickName.toString());
         if (ignoreMessage) {
@@ -311,7 +315,7 @@ public class StandaloneBot extends BotCore implements UpdatesListener, ChannelBo
             if (!(""+nCache).equals(""+nickName)) {
                 // Renamed
                 //members.put(chatId, from_id, nickName);
-                sayOnChannel(chatId, "теперь я знаю " + nCache + " как " + nickName + " \uD83D\uDE0E", originalMessage.messageId());
+                sayOnChannel(chatId, "теперь я знаю " + nCache + " как " + nickName + " \uD83D\uDE0E", originalMessageId);
                 //saveState();
             } else {
                 // Just update lastSeen
@@ -320,7 +324,7 @@ public class StandaloneBot extends BotCore implements UpdatesListener, ChannelBo
             }
         } else {
             members.put(chatId, from_id, nickName);
-            sayOnChannel(chatId, "теперь я знаю " + nickName + " \uD83D\uDE0E", originalMessage.messageId());
+            sayOnChannel(chatId, "теперь я знаю " + nickName + " \uD83D\uDE0E", originalMessageId);
             saveState();
         }
 
@@ -339,9 +343,9 @@ public class StandaloneBot extends BotCore implements UpdatesListener, ChannelBo
             }
         }
 
-        if (originalMessage.messageThreadId() != null) {
+        if (originalMessage != null && originalMessage.messageThreadId() != null) {
             log.info("*personal reply message*");
-            my = true;
+            // my = true;
         }
 
         if (adminSays) {
@@ -820,8 +824,13 @@ public class StandaloneBot extends BotCore implements UpdatesListener, ChannelBo
         return members.randomAt(chatId);
     }
 
-    private static String toJson(Update u) {
-        return "{ \"update_id\":\"" + u.updateId() + "\", \"message\":\"" + u.message() + "\", \"edited_message\":\"" + u.editedMessage() + "\", \"channel_post\":\"" + u.channelPost() + "\", \"edited_channel_post\":\"" + u.editedChannelPost() + "\", \"inline_query\":\"" + u.inlineQuery() + "\", \"chosen_inline_result\":\"" + u.chosenInlineResult() + "\", \"callback_query\":\"" + u.callbackQuery() + "\", \"shipping_query\":\"" + u.shippingQuery() + "\", \"pre_checkout_query\":\"" + u.preCheckoutQuery() + "\" }";
+    private static String toJson(Object u) {
+        try {
+            return JsonObjectMapper.getMapper().writeValueAsString(u);
+        } catch (JsonProcessingException e) {
+            // return "{ \"update_id\":\"" + u.updateId() + "\", \"message\":\"" + u.message() + "\", \"edited_message\":\"" + u.editedMessage() + "\", \"channel_post\":\"" + u.channelPost() + "\", \"edited_channel_post\":\"" + u.editedChannelPost() + "\", \"inline_query\":\"" + u.inlineQuery() + "\", \"chosen_inline_result\":\"" + u.chosenInlineResult() + "\", \"callback_query\":\"" + u.callbackQuery() + "\", \"shipping_query\":\"" + u.shippingQuery() + "\", \"pre_checkout_query\":\"" + u.preCheckoutQuery() + "\" }";
+            throw new RuntimeException(e);
+        }
     }
 
     private String rdbDeep(String source) {
